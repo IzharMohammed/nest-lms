9cb16c6de26372ba8533e0f77c7002f7f097a103
IzharMohammed <izharmohammed21@gmail.com>
2026-02-19 02:27:53 +0530

    Add prisma

---
+++ prisma/migrations/20260218205306_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fname" TEXT NOT NULL,
+    "lname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from 'src/auth/dto/registerUser-auth.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) { }
+
   async createUser(registerUserDto: RegisterDto) {
     try {
-
+      return await this.prisma.user.create({
+        data:
+        {
+          email: registerUserDto.email,
+          fname: registerUserDto.fname,
+          lname: registerUserDto.lname,
+          password: registerUserDto.password
+        }
+      })
     } catch (error) {
-
+      console.error(error)
     }
   }
 }
