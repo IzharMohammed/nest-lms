10c9e30e412f15ec1515ba5a79c768353a80687c
IzharMohammed <izharmohammed21@gmail.com>
2026-02-19 13:19:27 +0530

    Fixed prisma errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,6 @@ export class AuthService {
 
   @Post('register')
   async registerUser(registerUserDto: RegisterDto) {
-    console.log("registerUserDto", registerUserDto);
     const saltRounds = 10;
     const hash = await bcrypt.hash(registerUserDto.password, saltRounds);
     /**
@@ -18,9 +17,11 @@ export class AuthService {
      *  if user not exist then add email and hashed password db
      * 
      */
-    return await this.usersService.createUser({
+    const user = await this.usersService.createUser({
       ...registerUserDto,
       password: hash
     })
+    console.log("user", user);
+    return user;
   }
 }
